6bd04686a0244739ce765f37b72dbc6bac808069
Dan Kew <43997561+epeeme@users.noreply.github.com>
2019-11-19 17:06:50 +0000

    Club active state

--- main/classes/club.class.php
+++ main/classes/club.class.php
@@ -18,7 +18,7 @@ class club extends DB {
         $res['data'] = [];
         $sql = $this->db->prepare('SELECT fencerFirstname, fencerSurname, fencerFullname, fencerID, yob, fencers.country, 
                                           DATE_FORMAT(MAX(fullDate),\'%d-%m-%Y\') AS LastRep, DATE_FORMAT(MIN(fullDate),\'%d-%m-%Y\') AS FirstRep, 
-                                          count(results.dateID) AS TimesRep
+                                          count(results.dateID) AS TimesRep, region, cty, sch, uni
                                    FROM results 
                                    INNER JOIN eventDates ON eventDates.ID = results.dateID
                                    INNER JOIN clubs ON clubs.ID = fencerClubID 
@@ -30,7 +30,34 @@ class club extends DB {
 
         $clubData = $sql->fetchAll(PDO::FETCH_ASSOC);      
 
+        $sql = $this->db->prepare('SELECT region, cty, sch, uni
+                                   FROM clubs
+                                   WHERE ID = :cID');
+        $sql->bindValue(":cID", $data['cID']);
+        $sql->execute();
+
+        $clubInfo = $sql->fetch(PDO::FETCH_ASSOC);      
+
+        if ($clubInfo['region'] == 1) $clubType = ' AND region = 1';
+            else if ($clubInfo['cty'] == 1) $clubType = ' AND cty = 1';
+            else if ($clubInfo['sch'] == 1) $clubType = ' AND sch = 1';
+            else if ($clubInfo['uni'] == 1) $clubType = ' AND uni = 1';
+            else $clubType = 'AND (region = 0 AND cty = 0 AND sch = 0 AND uni = 0)';
+
         foreach ($clubData as $row) {
+            
+            $sql = $this->db->prepare('SELECT fencerClubID, fullDate
+                                       FROM results 
+                                       INNER JOIN eventDates ON results.dateID = eventDates.ID 
+                                       INNER JOIN clubs ON clubs.ID = fencerClubID 
+                                       WHERE fencerID = :fencerID '.$clubType.'
+                                       ORDER BY eventDates.fullDate DESC LIMIT 0,1');
+            $sql->bindValue(":fencerID", $row['fencerID']);
+            $sql->execute();
+
+            $clubsData = $sql->fetch(PDO::FETCH_ASSOC);      
+
+            $row['Active'] = $clubsData['fencerClubID'] == $data['cID'] ? true : false;
             $row['blank'] = '';
             $res['data'][] = $row;
         }
